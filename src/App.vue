9a8508be3a1552e9c9699ce0196b3dacc373a680
fabriziopace <pacefabriziowork@gmail.com>
2020-02-01 16:36:05 +0100

    Add more pictures

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app" class="container">
-      <div class="row">
       <ApodCards :dataPhoto="dataPhoto" class="align-self-center" />
-    </div>
   </div>
 </template>
 
 <script>
     import ApodCards from './components/ApodCards.vue';
     import axios from 'axios';
+    import moment from 'moment';
 
     export default {
         name: 'app',
@@ -21,16 +20,31 @@
             }
         },
         created: function() {
-            this.fetchApod();
+            let daysArray = this.getWeekDays();
+            for (let d = 0; d < daysArray.length; d++) {
+                this.fetchApod(daysArray[d]);
+            }
         },
         methods: {
-            fetchApod: function() {
-                let urlApi = 'https://api.nasa.gov/planetary/apod?api_key=' + process.env.VUE_APP_NASA_API_KEY;
+            fetchApod: function(date) {
+                if (!date) {
+                    date = moment().format('YYYY-MM-DD')
+                }
+                let urlApi = 'https://api.nasa.gov/planetary/apod?api_key=' + process.env.VUE_APP_NASA_API_KEY + '&date=' + date;
 
                 axios.get(urlApi)
                     .then((res) => {
                         this.dataPhoto.push(res.data);
                     });
+            },
+            getWeekDays: function() {
+                let daysArray = [];
+                let date = moment().isoWeek(1).startOf('week');
+                for (let d = 0; d < 7; d++) {
+                    daysArray.push(date.format('YYYY-MM-DD'));
+                    date.add(1, 'day');
+                }
+                return daysArray;
             }
         }
     }
--- src/components/ApodCards.vue
+++ src/components/ApodCards.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="container">
-        <div v-for="d in dataPhoto" :key="d.url" class="card text-white bg-dark mb-3">            
+    <div class="row">
+        <div v-for="d in dataPhoto" :key="d.url" class="card text-white bg-dark astronomy-card col-md-4">            
             <img :src="d.url" class="card-img-top" :alt="d.title">
              <div class="card-body">
-                <h5 class="card-title text-center">{{d.title}}</h5> 
+                <h5 class="card-title">{{d.title}}</h5> 
+                <!-- <p class="card-text">{{d.explanation}}</p> -->
+                <p class="card-text"><small class="text-muted">{{d.date}}</small></p>
             </div> 
         </div>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -17,6 +19,13 @@
 
 <style scoped>
     .astronomy-card {
-        border-radius: 50%;
+        padding: 0;
+        border-radius: 1%;
+        opacity: 0.9;
+        width: auto;
+    }
+    
+    .card-title {
+        font-size: 1rem;
     }
 </style>
